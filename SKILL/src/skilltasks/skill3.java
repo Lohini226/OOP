9ca4d5ef7964071f90a52aaaae1eedc6e8e4bd35
Lohini226 <2420030226@klh.edu.in>
2025-08-02 11:20:30 +0530

    Add files via upload

---
+++ SKILL/src/skilltasks/skill3.java
@@ -0,0 +1,15 @@
+package skilltasks;
+
+public class skill3 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int i = 1; i <= 5; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i + " is Even");
+            } else {
+                System.out.println(i + " is Odd");
+            }
+        }
+	}
+
+}
---
+++ SKILL/src/skilltasks/skill4.java
@@ -0,0 +1,23 @@
+package skilltasks;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.LocalDateTime;
+
+public class skill4 {
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			
+			
+			   
+			        LocalDate date = LocalDate.now();
+			        LocalTime time = LocalTime.now();
+			        LocalDateTime dateTime = LocalDateTime.now();
+
+			        System.out.println("Date: " + date);
+			        System.out.println("Time: " + time);
+			        System.out.println("Date & Time: " + dateTime);
+			    }
+
+		}
+
